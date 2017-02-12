c7809f56c45f0c7112087f38e6999662ada0f99a
Jamie Cone <jcone@e1z3r9p17.42.us.org>
2017-02-11 18:19:28 -0800

    removed an unessary file and improved my line some. code still needs some more cleaning in parts.

--- playing_with_pixels.c
+++ playing_with_pixels.c
@@ -6,7 +6,7 @@
 /*   By: jcone <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 12:56:04 by jcone             #+#    #+#             */
-/*   Updated: 2017/02/11 17:56:25 by jcone            ###   ########.fr       */
+/*   Updated: 2017/02/11 18:18:00 by jcone            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -856,6 +856,7 @@ int draw_line(void *mlx, void *win, int x, int y, int x2, int y2, int color, int
 	y_count = y;
 	increase_y = 1;
 	increase_x = 1;
+	calculate_increase_for_line(&increase_x, &increase_y, x, y, x2, y2);
 	if (thickness > 1)
 	{
 		hold_t_start = (x_count - (thickness / 2));
@@ -865,7 +866,7 @@ int draw_line(void *mlx, void *win, int x, int y, int x2, int y2, int color, int
 		hold_t_start = 0;
 	if (transparency == 0)
 	{
-		if (increase_x != 1 && increase_x >= 0)
+/*		if (increase_x >= 0)
 			{
 				t_count = hold_t_start + 1;
 				while (t_count <= hold_t_start + increase_x)
@@ -874,7 +875,7 @@ int draw_line(void *mlx, void *win, int x, int y, int x2, int y2, int color, int
 					t_count += 1;
 				}
 			}
-			if (increase_y != 1 && increase_y >= 0)
+			if (increase_y >= 0)
 			{
 				t_count = hold_t_start + 1;
 				while (t_count <= hold_t_start + increase_y)
@@ -883,7 +884,7 @@ int draw_line(void *mlx, void *win, int x, int y, int x2, int y2, int color, int
 					t_count += 1;
 				}
 			}
-			if (increase_x != -1 && increase_x < 0)
+			if (increase_x < 0)
 			{
 				t_count = hold_t_start + 1;
 				while (t_count >= hold_t_start + increase_x)
@@ -892,7 +893,7 @@ int draw_line(void *mlx, void *win, int x, int y, int x2, int y2, int color, int
 					t_count -= 1;
 				}
 			}
-			if (increase_y != -1 && increase_y < 0)
+			if (increase_y < 0)
 			{
 				t_count = hold_t_start + 1;
 				while (t_count >= hold_t_start + increase_y)
@@ -900,21 +901,26 @@ int draw_line(void *mlx, void *win, int x, int y, int x2, int y2, int color, int
 					draw_line(mlx, win, x, y - 1, x2, y2 - 1, color, thickness, 1);
 					t_count -= 1;
 				}
-			}
+			}*/
+		/*
+		 * some times it should change based on y other times x but never both!!!
+		 * the bellow code looks close but not quite there.
+		 * also need to fix it so the blue is a bit more present
+		 * (determine when to repeate a call of the sold color and how many times)
+		 */
 		color_temp = 0x80000000 + color;
-	//	if (increase_x != 0)
-	//		draw_line(mlx, win, x + 1, y, x2 + 1, y2, color, thickness, 1);
-	//	else if (increase_y == 1)
-	//		draw_line(mlx, win, x, y + 1, x2, y2 + 1, color, thickness, 1);
-	//	if (increase_y != 0)
-	//		draw_line(mlx, win, x, y + 2, x2, y2 + 2, color_temp, thickness, 1);
-	//	if (increase_x != 0)
-	//		draw_line(mlx, win, x + 2, y, x2 + 2, y2, color_temp, thickness, 1);
+		if (increase_x != 0)
+			draw_line(mlx, win, x + 1, y, x2 + 1, y2, color, thickness, 1);
+		else if (increase_y == 1)
+			draw_line(mlx, win, x, y + 1, x2, y2 + 1, color, thickness, 1);
+		if (increase_y == 1)
+			draw_line(mlx, win, x, y + 2, x2, y2 + 2, color_temp, thickness, 1);
+		if (increase_x != 0)
+			draw_line(mlx, win, x + 2, y, x2 + 2, y2, color_temp, thickness, 1);
 	//	draw_line(mlx, win, x + 1, y + 1, x2 + 1, y2 + 1, color_temp, thickness, 1);
 	}
 	else
 		color_temp = color;
-	calculate_increase_for_line(&increase_x, &increase_y, x, y, x2, y2);
 //	if (transparency == 0 && (repeate < fabs(increase_y) - 2 || repeate < fabs(increase_x) - 2))
 //	{
 //		repeate++;
